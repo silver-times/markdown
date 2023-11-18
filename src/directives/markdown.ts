47999ecf91c50c07700324bfc09ad50bd822d10f
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2023-11-18 14:25:20 +0100

    fix: refactor markdown.ts file

--- src/directives/markdown.ts
+++ src/directives/markdown.ts
@@ -1,45 +1,79 @@
 import { Directive } from "vue";
 
-const rules = [
-  [/######\s?([^\n]+)/g, "<h6>$1</h6>\n"],
-  [/#####\s?([^\n]+)/g, "<h5>$1</h5>\n"],
-  [/####\s?([^\n]+)/g, "<h4>$1</h4>\n"],
-  [/###\s?([^\n]+)/g, "<h3>$1</h3>\n"],
-  [/##\s?([^\n]+)/g, "<h2>$1</h2>\n"],
-  [/#\s?([^\n]+)/g, "<h1>$1</h1>\n"],
+interface Rule {
+  pattern: RegExp;
+  template: (...matches: string[]) => string;
+}
 
-  [/(?:!\[([^\]]+)\]\(([^)]+)\))/g, '<img alt="$1" src="$2" />'],
-  [
-    /\[video\s+([^\]]+)\]\(([^)]+)\)/g,
-    '<video controls alt="$1" src="$2"></video>',
-  ],
-  [/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>'],
-
-  [
-    /\[row\](?:\[col-left\]([^\]]+)\[\/col-left\]\[col-right\]([^\]]+)\[\/col-right\])+\[\/row\]/g,
-    "<tr><td>$1$2</td></tr>",
-  ],
-  [
-    /\[col-left\]([^\]]+)\[\/col-left\]/g,
-    '<div style="display: inline-block; margin-right: 10px;">$1</div>',
-  ],
-  [
-    /\[col-right\]([^\]]+)\[\/col-right\]/g,
-    '<div style="display: inline-block;">$1</div>',
-  ],
-  [
-    /\*\*([^*]+)\*\*/gm,
-    (match: any, content: any) => `<strong>${content}</strong>`,
-  ],
-  [/\*([^\n]+)\*/g, "<em>$1</em>"],
-  [/_([^\n]+)_/gm, "<u>$1</u>\n"],
+const rules: Rule[] = [
+  {
+    pattern: /######\s?([^\n]+)/g,
+    template: (_, content) => `<h6>${content}</h6>\n`,
+  },
+  {
+    pattern: /#####\s?([^\n]+)/g,
+    template: (_, content) => `<h5>${content}</h5>\n`,
+  },
+  {
+    pattern: /####\s?([^\n]+)/g,
+    template: (_, content) => `<h4>${content}</h4>\n`,
+  },
+  {
+    pattern: /###\s?([^\n]+)/g,
+    template: (_, content) => `<h3>${content}</h3>\n`,
+  },
+  {
+    pattern: /##\s?([^\n]+)/g,
+    template: (_, content) => `<h2>${content}</h2>\n`,
+  },
+  {
+    pattern: /#\s?([^\n]+)/g,
+    template: (_, content) => `<h1>${content}</h1>\n`,
+  },
+  {
+    pattern: /(?:!\[([^\]]+)\]\(([^)]+)\))/g,
+    template: (_, alt, src) => `<img alt="${alt}" src="${src}" />`,
+  },
+  {
+    pattern: /\[video\s+([^\]]+)\]\(([^)]+)\)/g,
+    template: (_, alt, src) =>
+      `<video controls alt="${alt}" src="${src}"></video>`,
+  },
+  {
+    pattern: /\[([^\]]+)\]\(([^)]+)\)/g,
+    template: (_, text, href) => `<a href="${href}">${text}</a>`,
+  },
+  {
+    pattern: /\[table\]([\s\S]*?)\[\/table\]/g,
+    template: (_, content) => `<table>${content}</table>`,
+  },
+  {
+    pattern: /\[row\]([\s\S]*?)\[\/row\]/g,
+    template: (_, content) => `<tr>${content}</tr>`,
+  },
+  {
+    pattern: /\[col-left\]([^\]]+)\[\/col-left\]/g,
+    template: (_, content) =>
+      `<td style="display: inline-block; margin-right: 10px;">${content}</td>`,
+  },
+  {
+    pattern: /\[col-right\]([^\]]+)\[\/col-right\]/g,
+    template: (_, content) =>
+      `<td style="display: inline-block;">${content}</td>`,
+  },
+  {
+    pattern: /\*\*([^*]+)\*\*/gm,
+    template: (_, content) => `<strong>${content}</strong>`,
+  },
+  { pattern: /\*([^\n]+)\*/g, template: (_, content) => `<em>${content}</em>` },
+  { pattern: /_([^\n]+)_/gm, template: (_, content) => `<u>${content}</u>\n` },
 ];
 
 const markdownDirective: Directive = {
   beforeMount(el: HTMLElement) {
     let html = el.innerText;
-    rules.forEach(([rule, template]) => {
-      html = html.replace(rule, template as string);
+    rules.forEach(({ pattern, template }) => {
+      html = html.replace(pattern, template);
     });
     el.innerHTML = html;
   },
